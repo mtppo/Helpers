d78c21d8e9110368d84673229db29ceef1d419ba
marcio.tavares <marcio.tavares@pagtel.com.br>
2019-09-30 07:56:15 -0300

    Inicial

---
+++ EncryptPhoneNumbers/EncryptPhoneNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using EncryptPhoneNumbers.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptPhoneNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Processamento de encriptação de números telefônicos.");
+            Console.WriteLine("INICIANDO...");
+
+            List<string> numbersFormat = new List<string>();
+            string url = "https://www.xpto.com.br/c/";
+            string saida = string.Empty;
+
+            var numbers = Files.GetNumbers();
+
+            foreach (var _numero in numbers)
+            {
+                string _formatado = _numero + "," + url + Base36.Encode(Convert.ToInt64(_numero)) + ";";
+                numbersFormat.Add(_formatado);
+            }
+
+            Files.SalveFile(numbersFormat);
+
+            Console.WriteLine("Convercao: {0}", saida);
+            Console.WriteLine("Fim do processamento");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EncryptPhoneNumbers/EncryptPhoneNumbers/Utils/Files.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptPhoneNumbers.Utils
+{
+    public static class Files
+    {
+        public static List<string> GetNumbers()
+        {
+            List<string> numbers = new List<string>();
+            string filePath = @"C:\ArquivosETL\ReadFile.csv";
+            StreamReader reader = new StreamReader(File.OpenRead(filePath));            
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    numbers.Add(line);
+                }
+            }
+
+            numbers.Add("var numbers");
+
+            return numbers;
+        }
+
+        public static bool SalveFile(List<string> numbersEncrypt)
+        {
+            bool validate = false;
+            string newLine = string.Empty;
+            string path = @"C:\ArquivosETL";
+            string filePath = @"C:\ArquivosETL\CreateFile.csv";
+
+            var csv = new StringBuilder();
+
+            try
+            {
+                foreach (var item in numbersEncrypt)
+                {
+                    newLine = string.Format("{0}", item);
+                    csv.AppendLine(newLine);
+                }
+
+                csv.AppendLine(newLine);
+
+                using (FileStream fs = File.Create(filePath))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                    fs.Write(info, 0, info.Length);
+                }
+
+                File.WriteAllText(filePath, csv.ToString());
+                validate = true;
+            }
+            catch (Exception ex)
+            {
+                validate = false;
+            }
+
+            return validate;
+        }
+    }
+}
